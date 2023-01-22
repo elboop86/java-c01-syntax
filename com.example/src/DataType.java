e27ef08ed243b8a8a0be6023f2bfda405180c13e
elboop86 <javi86medina@hotmail.com>
2023-01-22 23:21:21 +0100

    Initial commit

---
+++ com.example/src/DataType.java
@@ -0,0 +1,46 @@
+public class DataType {
+
+        public static void  main(String[] args) {
+
+            /*
+             Formato de nombrado en Java: camelCase
+             Formato de nombrado Phyton: snake_case
+             Formato de nombrado en C#: PascalCase
+
+             */
+
+            //1. numéricos enteros
+            byte number1 = 127;
+            short number2 = 2;
+            int number3 = 500000;
+            long number4 =334343434343L;
+            System.out.println(number1);
+            System.out.println(number2);
+            System.out.println(number3);
+            System.out.println(number4);
+
+
+            // 2. numéricos decimales
+            float number5 = 19.99f;
+            double number6 = 129.9999999d;
+            System.out.println(number5);
+            System.out.println(number6);
+
+            //3.Booleanos
+
+            boolean validated = true;
+            boolean isValid = false;
+            System.out.println(validated);
+            System.out.println(isValid);
+
+            //textos
+            char caracterA = 'a';
+            String text = "Hola mundo";
+            System.out.println(text);
+
+
+
+
+
+        }
+}
